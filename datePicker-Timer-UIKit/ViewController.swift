ec765721e881556bf05131add46622bb40800cb7
Carrington <Manyuchi>
2023-07-08 10:42:59 +0200

    we user the date picker to perform a count down

--- datePicker-Timer-UIKit/ViewController.swift
+++ datePicker-Timer-UIKit/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //ui objects
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var timerPicker: UIDatePicker!
+    
+    var after = Int()
+    var remind = Int()
+    var  counterInterval = TimeInterval()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        counterInterval = timerPicker.countDownDuration
+        remind = Int(counterInterval)
+        after = Int(counterInterval) - remind / 60
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
-
+    
+    
+    @objc func updateTimer() {
+        
+        //called every 1 sec to update timer
+        after -=  1
+        
+        // calculating hours, mins, secs
+        let hours = after / (60 * 60)
+        let mins = (after/60) - (hours*60)
+        let secs = (after - (60 * mins) - (60 * hours * 60))
+        
+        let timerString = String.init(format: "%2u : %2u : %2u  ", hours, mins, secs)
+        
+        myLabel.text = timerString
+        
+    }
 
 }
 
